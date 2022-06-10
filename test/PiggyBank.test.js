60cf9cae0b3b599f6ae527f9da75dfb352f79912
Jean Gatt <jean@bamboo.mt>
2022-06-10 10:16:21 +0200

    updated git ignore

--- test/PiggyBank.test.js
+++ test/PiggyBank.test.js
@@ -1,3 +1,5 @@
+const { time } = require("@openzeppelin/test-helpers");
+
 var PiggyBank = artifacts.require("PiggyBank");
 
 const ERROR_MSG = 'Not enough time has passed to withdraw the donations';
@@ -10,6 +12,13 @@ contract('PiggyBank', function(accounts){
     } catch(err){
       assert.include(err.message, ERROR_MSG, "The error message should contain not enough time.")
     }
+  });
+
+  it('Should test that the piggy bank can be broken after a minute', async function(){
+    let duration = time.duration.minutes(1);
+    let instance = await PiggyBank.deployed();
+    await time.increase(duration);
+    let breakPiggyBank = await instance.breakPiggyBank.call();
   });
 
 });
